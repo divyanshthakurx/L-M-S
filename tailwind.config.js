42d9215fcd53a550d71166cd7329fedcec7c2a10
Divyansh <divyanshthakurx@gmail.com>
2024-06-21 15:22:49 +0530

    built the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: { 
     extend: {
+      backgroundImage: {
+        'hero-pattern': "ur')",
+      },
+      fontFamily: {
+        serif: ['Times New Roman', 'serif'],
+      },
+      colors: {
+        'custom-dark-teal': 'rgba(0, 79, 79, 0.9)',
+        'custom-light-teal': 'rgba(10, 67, 61, 0.7)',
+        'custom-light-gray': 'rgba(240, 245, 245, 0.5)',
+      },
+      fontFamily: {
+        'outfit': 'Outfit, serif, sans-serif',
+      }
     },
   },
   plugins: [],
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
